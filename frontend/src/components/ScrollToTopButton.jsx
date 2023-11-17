987692aaf73885363f48e952d31c64a0fa6a8a56
Himel Majumder <himelmajumder1@gmail.com>
2023-11-17 21:55:56 +0100

    Add scroll functionality to ScrollToTopButton component

--- frontend/src/components/ScrollToTopButton.jsx
+++ frontend/src/components/ScrollToTopButton.jsx
@@ -1,4 +1,18 @@
+import { useState, useEffect } from "react";
+
 export function ScrollToTopButton() {
+  const [isScrolled, setIsScrolled] = useState(false);
+  const handleScroll = () => {
+    const scrolled = window.scrollY > 100;
+    setIsScrolled(scrolled);
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
   const handleClick = () => {
     window.scrollTo({
       top: 0,
@@ -8,12 +22,14 @@ export function ScrollToTopButton() {
 
   return (
     <div className="fixed bottom-8 right-8">
-      <button
-        onClick={handleClick}
-        className="bg-blue-200 px-3 py-2 rounded-lg inline-block hover:bg-blue-400 hover:cursor-pointer text-zinc-950 font-bold text-center"
-      >
-        <span className="material-symbols-outlined">expand_less</span>
-      </button>
+      {isScrolled && (
+        <button
+          onClick={handleClick}
+          className="bg-blue-200 px-3 py-2 rounded-lg inline-block hover:bg-blue-400 hover:cursor-pointer text-zinc-950 font-bold text-center"
+        >
+          <span className="material-symbols-outlined">expand_less</span>
+        </button>
+      )}
     </div>
   );
 }
